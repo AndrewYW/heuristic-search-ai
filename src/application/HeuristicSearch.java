55bc591718649e9f096d5b95823e04dfc969f200
Andrew Wang <andrewwang@nbp-106-250.nbp.ruw.rutgers.edu>
2017-11-16 23:23:58 -0500

    set up radiobuttons

--- src/application/HeuristicSearch.java
+++ src/application/HeuristicSearch.java
@@ -1,5 +1,6 @@
 package application;
 
+import application.view.HeuristicController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,16 +11,33 @@ public class HeuristicSearch extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader();
+        /**
+         * FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/view/Heuristic.fxml"));
         AnchorPane root = loader.load();
         primaryStage.setTitle("Heuristic Search - Andrew Wang & Mauricio Alvarez");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+         **/
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/Heuristic.fxml"));
+        //loader.setLocation(getClass().getResource("view/Heuristic.fxml"));
+        AnchorPane rootLayout = loader.load();
+
+        HeuristicController hControl = loader.getController();
+        hControl.start(primaryStage);
+
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setTitle("Heuristic Search - Andrew Wang & Mauricio Alvarez");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
--- src/application/view/HeuristicController.java
+++ src/application/view/HeuristicController.java
@@ -1,4 +1,36 @@
 package application.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+
 public class HeuristicController {
+
+    @FXML
+    private RadioButton uniformButton;
+    @FXML
+    private RadioButton aStarButton;
+    @FXML
+    private RadioButton weightedButton;
+    @FXML
+    private RadioButton sequentialButton;
+
+    public void start(Stage primaryStage){
+        setToggle();
+    }
+
+    private void setToggle(){
+        final ToggleGroup group = new ToggleGroup();
+        uniformButton.setToggleGroup(group);
+        aStarButton.setToggleGroup(group);
+        weightedButton.setToggleGroup(group);
+        sequentialButton.setToggleGroup(group);
+
+        uniformButton.setSelected(true);
+    }
 }
