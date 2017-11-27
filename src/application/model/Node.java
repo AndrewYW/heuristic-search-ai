92ea7fe3cd7c59ad02fe4ac792c7548891a5d703
Andrew Wang <andrewwang@MacbookPros-MBP.home>
2017-11-27 02:29:43 -0500

    remove accidental Euclidean distance override, add some comments to node methods.

--- src/application/model/Node.java
+++ src/application/model/Node.java
@@ -49,9 +49,8 @@ public class Node implements Comparable<Node>{
 
     /**
      * Method to find the cost of travel between two nodes.
-     * @TODO
-     * @param n
-     * @return
+     * @param n The node that the current one is being compared to.
+     * @return  Float value representing cost of traversal.
      */
     public float getCost(Node n){
         char t1 = this.type;
@@ -166,16 +165,33 @@ public class Node implements Comparable<Node>{
         return 0;
     }
 
+    /**
+     * Getter method to access the successors of this node.
+     * @return ArrayList containing the successors.
+     */
     public ArrayList<Node> getChildren() { return this.children; }
 
+    /**
+     * Setter method to change f(this) value.
+     * @param value The float to set f(this) to.
+     */
     public void setfVal(float value){
         this.fVal = value;
     }
 
+    /**
+     * Setter method to change the g(this) value.
+     * @param value The float to set g(this) to.
+     */
     public void setgVal(float value){
         this.gVal = value;
     }
 
+    /**
+     * Method to choose the heuristic function.
+     * @param goal The goal node to reach.
+     * @param heuristic Integer representing function choice. 0 = Euclidean, 1 = Manhattan, 2 = Chebyshev, 3 = Octile.
+     */
     public void sethVal(Node goal, int heuristic){
         switch (heuristic){
             case 0:     //Euclidean distance
@@ -191,20 +207,21 @@ public class Node implements Comparable<Node>{
                 diagonal(goal, heuristic);
                 break;
         }
-        int row1 = this.row;
-        int col1 = this.col;
-        int row2 = goal.getRow();
-        int col2 = goal.getCol();
-
-        double aSquared = Math.pow((row1-row2), 2);
-        double bSquared = Math.pow((col1-col2), 2);
-
-        this.hVal =  (float)Math.sqrt(aSquared+bSquared);
     }
 
+    /**
+     * Method to set the parent of the given node.
+     * @param parent The node that is the parent of this node.
+     */
     public void setParent(Node parent){
         this.parent = parent;
     }
+
+    /**
+     * Method to set the successor nodes for this given node.
+     * Includes checks for outside the map border, and blocked nodes.
+     * @param graph The 2d Node array containing all nodes.
+     */
     public void setChildren(Node[][] graph){
         if(inBounds(row+1, col+1)) {
             if (graph[row + 1][col + 1].getType() != '0')
@@ -240,12 +257,24 @@ public class Node implements Comparable<Node>{
         }
     }
 
+    /**
+     * Helper method to check if a node is within the map.
+     * Used to add successors of the current node to ArrayList.
+     * @param row The row index of the potential successor.
+     * @param col The column index of the potential successor.
+     * @return    True if in bounds, false if not.
+     */
     private boolean inBounds(int row, int col){
         if (row > -1 && row < 120 && col > -1 && col < 160)
             return true;
         return false;
     }
 
+    /**
+     * Method override for the minheap implementation.
+     * @param n The node being compared to
+     * @return  -1 if f(this) < f(n), 1 if f(this) > f(n), 0 if equal.
+     */
     @Override
     public int compareTo(Node n){
         if(this.getfVal() < n.getfVal())
@@ -255,6 +284,10 @@ public class Node implements Comparable<Node>{
         return 0;
     }
 
+    /**
+     * Method to implement Euclidean distance for the heuristic function.
+     * @param node The goal node to reach.
+     */
     private void euclidean(Node node){
         int row1 = this.row;
         int col1 = this.col;
@@ -267,6 +300,11 @@ public class Node implements Comparable<Node>{
         this.hVal =  (float)Math.sqrt(aSquared+bSquared);
 
     }
+
+    /**
+     * Method to implement Manhattan distance for the heuristic function.
+     * @param node The goal node to reach.
+     */
     private void manhattan(Node node){
         int row1 = this.row;
         int col1 = this.col;
@@ -279,6 +317,11 @@ public class Node implements Comparable<Node>{
         this.hVal = x+y;
     }
 
+    /**
+     * Method to implement Diagonal distance for the heuristic function.
+     * @param node The goal node to reach.
+     * @param d    Choice between heuristic functions: Chebyshev vs Octile.
+     */
     private void diagonal(Node node, int d){
         double cost;
         if(d == 2) {          //Chebyshev distance
